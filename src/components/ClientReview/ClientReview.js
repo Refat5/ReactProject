92efee53abbe124b647a3fc329dc5b4dbe02fb3c
Refat5 <mdrefat382139.com>
2020-08-14 18:31:29 +0600

    create clientReview page

---
+++ src/components/ClientReview/ClientReview.js
@@ -0,0 +1,18 @@
+import React, {Component,Fragment} from 'react';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+class ClientReview extends Component {
+    render(){
+        return(
+            <Fragment>
+
+            </Fragment>
+
+        );
+    }
+   
+}
+
+
+export default  ClientReview;
