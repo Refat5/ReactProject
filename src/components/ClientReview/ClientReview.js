6078273b39f2052d0f8ed2c191a1e710de1ff19a
Refat5 <mdrefat382139.com>
2020-08-14 18:42:44 +0600

    client Review section add setting part

--- src/components/ClientReview/ClientReview.js
+++ src/components/ClientReview/ClientReview.js
@@ -4,6 +4,44 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 class ClientReview extends Component {
     render(){
+        var settings = {
+            autoplaySpeed:2000,
+            autoplay:true,
+            dots: true,
+            infinite: false,
+            speed: 2000,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            vertical:true,
+            verticalSwiping:true,
+            responsive: [
+              {
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  infinite: true,
+                  dots: true
+                }
+              },
+              {
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  initialSlide: 1
+                }
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              }
+            ]
+          };
+       
         return(
             <Fragment>
 
