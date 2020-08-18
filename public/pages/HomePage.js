058f631e092f496c1c94fbdf1c54913568aed4a8
Refat5 <mdrefat382139@gmail.com>
2020-08-18 18:55:02 +0600

    create home page

---
+++ public/pages/HomePage.js
@@ -0,0 +1,35 @@
+import React,{Component,Fragment} from 'react';
+import TopBanner from "../components/TopBanner/TopBanner";
+import Service from '../components/Service/Service';
+import TopNave from '../components/TopNave/TopNave';
+import Analysis from '../components/Analysis/Analysis';
+import Summary from '../components/Summary/Summary';
+import RecentProject from '../components/RecentProject/RecentProject';
+import Cources from '../components/Cources/Cources';
+import Video from '../components/video/Video';
+import ClientReview from '../components/ClientReview/ClientReview';
+import Footer from '../components/Footer/Footer'
+
+class HomePage extends Component {
+    render()
+    {
+        return(
+            <Fragment>
+                <TopNave/>
+                <TopBanner/>
+                <Service/>
+                <Analysis/>
+                <Summary/>
+                <RecentProject/>
+                <Cources/>
+                <Video/>
+                <ClientReview/>
+                <Footer/>
+
+            </Fragment>
+
+        );
+    }
+};
+
+export default HomePage;
