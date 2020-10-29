caffa1052e8bf787a67e973dd3fe08f67779ad11
Refat5 <mdrefat382139@gmail.com>
2020-10-29 17:20:13 +0600

    course Details page is Done

--- src/components/AllCourse/AllCourse.js
+++ src/components/AllCourse/AllCourse.js
@@ -1,78 +1,93 @@
-import React,{ Component, Fragment } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import React, { Component, Fragment } from 'react';
 
-import {Container,Row,Col} from 'react-bootstrap';
-import mobile from '../../asset/images/ad.jpg'
-import web from '../../asset/images/web.jpg'
-import graphic from '../../asset/images/gp.jpg'
-import normal from '../../asset/images/background.jpg'
+import { Link } from 'react-router-dom';
+import graphic from '../../asset/images/gp.jpg';
+import mobile from '../../asset/images/ad.jpg';
+import normal from '../../asset/images/background.jpg';
+import web from '../../asset/images/web.jpg';
 
-
-class AllCourse extends Component{
-    render(){
-        return(
+class AllCourse extends Component {
+    render() {
+        return (
             <Fragment>
-            <Container className="mt-5">
-                
-                <Row>
-                <div class="p-2 col-lg-6 col-md-12 col-sm-12">
-                    <div class="p-3 row">
-                        <div class="p-2 col-lg-6 col-md-6 col-sm-12">
-                            <img class="courseImg" src={mobile}/>
-                        </div>
+                <Container className="mt-5">
+                    <Row>
+                        <div class="p-2 col-lg-6 col-md-12 col-sm-12">
+                            <div class="p-3 row">
+                                <div class="p-2 col-lg-6 col-md-6 col-sm-12">
+                                    <img class="courseImg" src={mobile} />
+                                </div>
                                 <div class="p-2 col-lg-6 col-md-6 col-sm-12">
                                     <h5 class="text-justify courseTitle">Mobile Development</h5>
-                                    <p class="text-justify courseDes">I build native and cross platfrom mobile app for your business app for your business</p>
-                                    <a class="courseDetails float-left" href="">Details</a>
+                                    <p class="text-justify courseDes">
+                                        I build native and cross platfrom mobile app for your
+                                        business app for your business
+                                    </p>
+                                    <Link class="courseDetails float-left" to="/courseDetails">
+                                        Details
+                                    </Link>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                 </div>
-                 
-                 <div class="p-2 col-lg-6 col-md-12 col-sm-12">
-                    <div class="p-3 row">
-                        <div class="p-2 col-lg-6 col-md-6 col-sm-12">
-                            <img class="courseImg" src={web}/>
-                        </div>
+
+                        <div class="p-2 col-lg-6 col-md-12 col-sm-12">
+                            <div class="p-3 row">
+                                <div class="p-2 col-lg-6 col-md-6 col-sm-12">
+                                    <img class="courseImg" src={web} />
+                                </div>
                                 <div class="p-2 col-lg-6 col-md-6 col-sm-12">
                                     <h5 class="text-justify courseTitle">Web Development</h5>
-                                    <p class="text-justify courseDes">I build native and cross platfrom mobile app for your business app for your business</p>
-                                    <a class="courseDetails float-left" href="">Details</a>
+                                    <p class="text-justify courseDes">
+                                        I build native and cross platfrom mobile app for your
+                                        business app for your business
+                                    </p>
+                                    <Link class="courseDetails float-left" to="/courseDetails">
+                                        Details
+                                    </Link>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                 </div>
 
-                   
-                 <div class="p-2 col-lg-6 col-md-12 col-sm-12">
-                    <div class="p-3 row">
-                        <div class="p-2 col-lg-6 col-md-6 col-sm-12">
-                            <img class="courseImg" src={graphic}/>
-                        </div>
+                        <div class="p-2 col-lg-6 col-md-12 col-sm-12">
+                            <div class="p-3 row">
+                                <div class="p-2 col-lg-6 col-md-6 col-sm-12">
+                                    <img class="courseImg" src={graphic} />
+                                </div>
                                 <div class="p-2 col-lg-6 col-md-6 col-sm-12">
                                     <h5 class="text-justify courseTitle">Graphic</h5>
-                                    <p class="text-justify courseDes">I build native and cross platfrom mobile app for your business app for your business</p>
-                                    <a class="courseDetails float-left" href="">Details</a>
+                                    <p class="text-justify courseDes">
+                                        I build native and cross platfrom mobile app for your
+                                        business app for your business
+                                    </p>
+                                    <Link class="courseDetails float-left" to="/courseDetails">
+                                        Details
+                                    </Link>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                 </div>
-                   
-                 <div class="p-2 col-lg-6 col-md-12 col-sm-12">
-                    <div class="p-3 row">
-                        <div class="p-2 col-lg-6 col-md-6 col-sm-12">
-                            <img class="courseImg" src={normal}/>
-                        </div>
+
+                        <div class="p-2 col-lg-6 col-md-12 col-sm-12">
+                            <div class="p-3 row">
+                                <div class="p-2 col-lg-6 col-md-6 col-sm-12">
+                                    <img class="courseImg" src={normal} />
+                                </div>
                                 <div class="p-2 col-lg-6 col-md-6 col-sm-12">
                                     <h5 class="text-justify courseTitle">Web Development</h5>
-                                    <p class="text-justify courseDes">I build native and cross platfrom mobile app for your business app for your business</p>
-                                    <a class="courseDetails float-left" href="">Details</a>
+                                    <p class="text-justify courseDes">
+                                        I build native and cross platfrom mobile app for your
+                                        business app for your business
+                                    </p>
+                                    <Link class="courseDetails float-left" to="/courseDetails">
+                                        Details
+                                    </Link>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                 </div>
-                </Row>
-
-          
-            </Container>
-        </Fragment>
+                    </Row>
+                </Container>
+            </Fragment>
         );
     }
-
 }
 export default AllCourse;
--- src/router/AppROuter.js
+++ src/router/AppROuter.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import About from '../pages/About';
 import AllCourse from '../pages/AllCourse';
 import Contact from '../pages/ContactUs';
+import CourseDetails from '../pages/Course';
 import Home from '../pages/Home';
 import Portfolio from '../pages/PortfoliPage';
 import Privacy from '../pages/Privacy';
@@ -27,6 +28,7 @@ class AppROuter extends Component {
                     <Route exact path="/terms" component={Terms} />
                     <Route exact path="/privacy" component={Privacy} />
                     <Route exact path="/Project" component={Project} />
+                    <Route exact path="/courseDetails" component={CourseDetails} />
                 </Switch>
             </Fragment>
         );
